ced9c4c0c22bbf81757a9ee6ec6dbeeb2bdaac64
Amawin <amawinmusic@gmail.com>
2026-04-03 18:08:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// 1. Countdown Logic (3rd April 6PM)
-var countDownDate = new Date("April 3, 2026 18:00:00").getTime();
-
-var x = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    if(document.getElementById("days")) {
-        document.getElementById("days").innerHTML = days;
-        document.getElementById("hours").innerHTML = hours;
-        document.getElementById("minutes").innerHTML = minutes;
-        document.getElementById("seconds").innerHTML = seconds;
-    }
-
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("countdown-container").innerHTML = "<h3>DURGA IS OUT NOW!</h3>";
-    }
-}, 1000);
-
 // News Slider Controller
 let currentNewsIdx = 0;
 
